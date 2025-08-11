5eae32822a64949f17d2570bf79d14df1d975a9d
Josh-0115 <sunnyjosh0115@gmail.com>
2025-08-11 23:41:37 +0530

    fixed export problem for socket

--- backend/frontend/src/api/socket.js
+++ backend/frontend/src/api/socket.js
@@ -3,3 +3,4 @@ import { io } from "socket.io-client";
 const SOCKET_URL = "https://whatsapp-clone-n6sr.onrender.com"; // Render backend URL
 
 export const socket = io(SOCKET_URL);
+export default socket;
